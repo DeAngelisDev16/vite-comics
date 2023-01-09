241bc165ac6ce14346a91f768dead767f95f4fa9
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-09 16:44:25 +0100

    v-for header navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,57 @@
-<script setup>
+<script>
+export default {
+    data() {
+        return {
+            headerNavList: [
+                {
+                    text: 'Characters'
+                },
+                {
+                    text: 'Comics'
+                },
+                {
+                    text: 'Movies'
+                },
+                {
+                    text: 'TV'
+                },
+                {
+                    text: 'Games'
+                },
+                {
+                    text: 'Collectibles'
+                },
+                {
+                    text: 'Videos'
+                },
+                {
+                    text: 'Fans'
+                },
+                {
+                    text: 'News'
+                },
+                {
+                    text: 'Shop'
+                },
+            ]
+        }
+    }
+}
 
 </script>
 
 <template>
-    <h1>ciao ciao</h1>
+    <div class="header_wrapper">
+        <img src="../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li v-for="link in headerNavList">{{ link.text }}</li>
+        </ul>
+    </div>
 
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.header_wrapper {
+    display: flex;
+}
 </style>
