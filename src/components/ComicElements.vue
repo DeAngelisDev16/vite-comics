6f559ac0cde1505bee6040ccbc4b5fea9aced584
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-10 15:53:36 +0100

    Inserisco immagini dinamicamente in products

--- src/components/ComicElements.vue
+++ src/components/ComicElements.vue
@@ -22,7 +22,8 @@ export default {
 
 <template>
     <div class="comics-elements">
-        {{ imagePath }} -- {{ comicsPrice }} -- {{ comicsSeries }} -- {{ comicsType }}
+        <!-- {{ imagePath }} -- {{ comicsPrice }} -- {{ comicsSeries }} -- {{ comicsType }} -->
+        <img :src="imagePath" alt="image path">
     </div>
 
 
@@ -33,6 +34,11 @@ export default {
     color: white;
     border: 2px solid white;
     width: calc(100% / 6);
+    padding: 1rem;
+
+    img {
+        width: 100%;
+    }
 
 }
 </style>
