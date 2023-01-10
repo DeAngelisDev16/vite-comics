24040330e3cb1f75146ad85e574dae6a6ca44ef8
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-10 09:22:16 +0100

    Upper Footer lists commit

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,23 +12,58 @@ export default {
             <div class="List-one">
                 <h2>DC COMICS</h2>
                 <ul>
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>TV</li>
-                    <li>Games</li>
-                    <li>Videos</li>
-                    <li>News</li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
                 </ul>
 
                 <h2>Shop</h2>
                 <ul>
-                    <li>Shop DC</li>
-                    <li>Shop DC Collectibles</li>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
                 </ul>
 
             </div>
-            <div class="list-two"></div>
+            <div class="list-two">
+                <h2>DC</h2>
+                <ul>
+                    <li><a href="#">Terms of use</a></li>
+                    <li><a href="#">Privacy policy (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+
+                </ul>
+            </div>
+            <div class="list-three">
+                <h2>SITES</h2>
+                <ul>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
+
+
+
+
+
+
+
+
+
 
 
         </div>
@@ -58,5 +93,20 @@ export default {
     width: 80%;
     margin: auto;
     padding: 1rem;
+    display: flex;
+
+    h2 {
+        margin-left: 2rem;
+    }
+
+    ul {
+        margin-left: 2rem;
+        list-style: none;
+
+        a {
+            text-decoration: none;
+        }
+    }
+
 }
 </style>
